bbd31530492d3d9a4c9d9d1e91c90e8d500eb748
Alreinz <lorenzdiz@gmail.com>
2017-06-11 09:40:19 +0800

    (Blaise) added database connection

--- main.py
+++ main.py
@@ -2,8 +2,31 @@ from flask import Flask
 from flask import send_file
 from flask import request
 from flask import json
+import os
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
+
+# Database Models
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    title = db.Column(db.String(128), unique=False)
+    description = db.Column(db.String(256), unique=False)
+    upvotes = db.Column(db.Integer)
+    downvotes = db.Column(db.Integer)
+    tag = db.Column(db.String(64))
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
 
 @app.route('/', methods=['GET', 'POST'])
 def init():
